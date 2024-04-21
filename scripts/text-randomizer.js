316d83e096a465eb67e378ac4d7193faefd7fc1a
Aurorinae <aurorinae@outlook.com>
2024-04-21 06:08:10 -0600

    Funcional

--- scripts/text-randomizer.js
+++ scripts/text-randomizer.js
@@ -1,42 +1,63 @@
 let usedIndexes = []; // Registro de índices utilizados
 
-function selectRandomPhrase(archivo) {
-    fetch(archivo)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al leer el archivo.');
-            }
-            return response.text();
-        })
-        .then(data => {
-            let lineas = data.split('\n');
+function selectRandomPhrase(archivo, keyPressedId) {
+  fetch(archivo)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al leer el archivo.");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      let lineas = data.split("\n");
 
-            // Verificar si se han utilizado todos los índices, en cuyo caso, restablecer el registro
-            if (usedIndexes.length === lineas.length) {
-                usedIndexes = [];
-            }
+      // Verificar si se han utilizado todos los índices, en cuyo caso, restablecer el registro
+      if (usedIndexes.length === lineas.length) {
+        usedIndexes = [];
+      }
 
-            let index;
-            do {
-                index = Math.floor(Math.random() * lineas.length);
-            } while (usedIndexes.includes(index)); // Evitar índices repetidos
+      let index;
+      console.log(keyPressedId);
+      if (keyPressedId == "before") {
+        if (usedIndexes.length > 1) {
+          usedIndexes.pop(); // corregir
+          index = usedIndexes[usedIndexes.length - 1];
+        } else {
+          return; // Corregir
+        }
+      } else if (keyPressedId == "next") {
+        do {
+          index = Math.floor(Math.random() * lineas.length);
+        } while (usedIndexes.includes(index)); // Evitar índices repetidos
 
-            usedIndexes.push(index); // Registrar el nuevo índice utilizado
+        usedIndexes.push(index); // Registrar el nuevo índice utilizado
+      } else {
+        console.log("Something went wrong!");
+      }
 
-            let lineaAleatoria = lineas[index];
-            putPhraseInBox('frase', lineaAleatoria);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+      let lineaAleatoria = lineas[index];
+      let box = document.getElementById("frase");
+      box.innerHTML = lineaAleatoria; // Corregir aquí
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
-function putPhraseInBox(elementId, frase) {
-    let box = document.getElementById(elementId);
-    box.innerHTML = frase; // Corregir aquí
-}
+// document.body.addEventListener('click', (e) => {
+//     console.log("Clic en el cuerpo de la página.");
+//     selectRandomPhrase('/frases.txt');
+// });
 
-document.body.addEventListener('click', (e) => {
-    console.log("Clic en el cuerpo de la página.");
-    selectRandomPhrase('/frases.txt');
+document.getElementById("before").addEventListener("click", (e) => {
+  selectRandomPhrase("/frases.txt", e.currentTarget.id);
 });
+
+document.getElementById("next").addEventListener("click", (e) => {
+  selectRandomPhrase("/frases.txt", e.currentTarget.id);
+});
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  selectRandomPhrase("/frases.txt", "next");
+});
+
