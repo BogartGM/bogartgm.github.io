6edd68268ba0d99313cb253d9e272ae7377f5158
Aurorinae <aurorinae@outlook.com>
2024-05-17 17:49:45 -0600

    detener sonido

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var _a;
 import { Elements } from "./dom-elements.js";
-import { stopSound } from "../js/generarSonido.js";
+import { stopSound } from "../js/generarSonido/generar-sonido.js";
 import { anadirTarjetaRentado } from "./rentedCardComputer/anadirTarjetaRentado.js";
 import { addTimeWheelSelector } from "./timeWheelSelector/add-time-wheel-selector.js";
 addTimeWheelSelector(Elements.inputHourSelector);
@@ -8,3 +8,9 @@ addTimeWheelSelector(Elements.inputMinuteSelector);
 addTimeWheelSelector(Elements.inputRentPrice);
 (_a = Elements.btnRentar) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => anadirTarjetaRentado());
 (_a = Elements.btnPanico) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => stopSound());
+
+let modal = document.getElementById("exampleModal");
+modal.addEventListener('hidden.bs.modal', function () {
+  // Aquí puedes agregar el código que deseas ejecutar cuando se cierra el modal
+  stopSound();
+});
