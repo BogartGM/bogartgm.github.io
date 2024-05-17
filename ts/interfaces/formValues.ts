2573490116984a89ce56d40016a944914cbdc84d
Aurorinae <aurorinae@outlook.com>
2024-05-17 17:25:41 -0600

    sdfsdf

--- ts/interfaces/formValues.ts
+++
@@ -1,5 +0,0 @@
-export interface FormValues {
-  computerName: string;
-  rentPrice: string; // Rent price per hour in dollars (e.g., "$10")
-  tiempoPedido: string; // Requested time in format "HH:MM:SS"
-}
--- ts/main.ts
+++
@@ -1,8 +0,0 @@
-import { Elements } from "./dom-elements.js";
-import { anadirTarjetaRentado } from "./rentedCardComputer/anadirTarjetaRentado.js";
-import { addTimeWheelSelector } from "./timeWheelSelector/add-time-wheel-selector.js";
-
-addTimeWheelSelector(Elements.inputHourSelector);
-addTimeWheelSelector(Elements.inputMinuteSelector);
-addTimeWheelSelector(Elements.inputRentPrice);
-Elements.btnRentar?.addEventListener("click", () => anadirTarjetaRentado());
